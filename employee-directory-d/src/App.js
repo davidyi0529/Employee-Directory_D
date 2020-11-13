c999ec4ee5d0a3a25160591ba53f939392090c7f
David Yi <david.yi.0529@gmail.com>
2020-11-12 22:42:21 -0800

    header html design

--- employee-directory-d/src/App.js
+++ employee-directory-d/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Gallery from "./pages/Gallery";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div>
     <Header />
       <Navbar />
-      <Wrapper>
-        <Gallery />
-      </Wrapper>
+        <Wrapper>
+          <Container />
+        </Wrapper>
       <Footer />
     </div>
   );
--- employee-directory-d/src/components/Header/index.js
+++ employee-directory-d/src/components/Header/index.js
@@ -3,9 +3,12 @@ import "./style.css";
 
 function Header() {
   return (
-    <footer className="header">
-      <span>LinkedUp { new Date().getFullYear() }</span>
-    </footer>
+    <div className="header">
+      <header className="container">
+            <img className="mylogo" src="https://github.com/davidyi0529/Portfolio_D/raw/master/assets/images/logo.png?raw=true" alt="" />
+            <h1>Employee-Directory_D</h1>
+      </header>
+    </div>
   );
 }
 
